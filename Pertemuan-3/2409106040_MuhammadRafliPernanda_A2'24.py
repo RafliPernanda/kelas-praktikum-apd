a3a8e9b3187f8cae5f690f102310bb7075aa0456
RafliPernanda <raflipernanda212@gmail.com>
2024-09-18 20:41:56 +0800

    code pertemuan 3

---
+++ Pertemuan-3/2409106040_MuhammadRafliPernanda_A2'24.py
@@ -0,0 +1,56 @@
+# fitur = int(input("pilih fitur : "))
+# match fitur :
+#     case 1:
+#         a = int(input("Masukkan angka 1 : "))
+#         b = int(input("Masukkan angka 2 : "))
+#         c = a + b 
+#         print (f"hasil penjumlahan angka 1 dan angka 2 adalah {c}")
+#     case 2:
+#         a = int(input("Masukkan angka 1 : "))
+#         b = int(input("Masukkan angka 2 : "))
+#         c = a - b 
+#         print (f"hasil pengurangan angka 1 dan angka 2 adalah {c}")
+#     case 3:
+#         a = int(input("Masukkan angka 1 : "))
+#         b = int(input("Masukkan angka 2 : "))
+#         c = a * b 
+#         print (f"hasil perkalian angka 1 dan angka 2 adalah {c}")
+#     case 4:
+#         a = int(input("Masukkan angka 1 : "))
+#         b = int(input("Masukkan angka 2 : "))
+#         c = a / b 
+#         print (f"hasil pembagian angka 1 dan angka 2 adalah {c}")
+
+
+# buku = int(input("Masukkan jumlah buku = "))
+# harga = int(input("Masukkan total harga buku = "))
+# diskon = 0.20
+# total_harga = buku * harga
+
+
+# if buku >= 5 and total_harga > 100000:
+#     totalDiskon = total_harga * diskon
+#     HargaDiskon = total_harga - totalDiskon
+#     print(f"diskon yang diterima sebanyak {totalDiskon:.0f}")
+#     print(f"Anda harus membayar sebanyak {HargaDiskon:.0f} setelah mendapat diskon 20%")
+# else: print("anda tidak mendapat diskon")
+
+
+# Nilai = int(input("Masukkan Nilai= "))
+# if Nilai >= 0 and Nilai < 101 :   
+#     if Nilai >= 80 : 
+#         if Nilai >= 90 and Nilai <= 100 :
+#             print("Nilai anda A+")
+#         if Nilai >= 80 and Nilai <= 89 :
+#             print("Nilai anda A-")
+#     if Nilai >= 70 : 
+#         if Nilai >= 75 and Nilai <= 79 :
+#             print("Nilai anda B+")
+#         if Nilai >= 70 and Nilai <= 74 :
+#             print("Nilai anda B-")
+# else :
+#     print("kondisi tidak memenuhi syarat")
+
+# jenisKelamin = input("Masukkan jenis kelamin anda (L/P) :")
+# hasil = "Jenis Kelamin Laki-Laki" if jenisKelamin == "L" else "Jenis Kelamin Perempuan" if jenisKelamin == "P" else "Jenis Kelamin Anda Tidak Diketahui"
+# print(hasil)
